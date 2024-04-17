44629b4446360d833e6cdafde99a4138b7530ca8
Nam250502 <namnguyen250502@gmail.com>
2024-04-17 12:57:02 +0700

    itnit

---
+++ app/controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class ProductController {
+    public function listProducts() {
+        $database = new Database();
+        $db = $database->getConnection();
+
+        $product = new ProductModel($db);
+        $stmt = $product->readAll();
+
+        include_once 'app/views/product_list.php';
+    }
+}
+?>  
